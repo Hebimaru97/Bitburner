45abb5f68fa87027cc7749dc1a60e13dc0fe6a8e
Hebi <anubisposeidon@gmail.com>
2023-12-02 17:10:04 -0600

    Update prep.js
    resolve min v max discrepancy, reduce 1000 threads to 1 for post bitnode

--- prep.js
+++ prep.js
@@ -12,8 +12,8 @@ export function IsPrepped(ns, target) {
 export async function Prep(ns, target, attacker, threadsGr, threadsWk) {
   while (!IsPrepped(ns, target)) {
     let pids = [];
-    pids.push(ns.exec("bin.gw.js", attacker, Math.min(threadsGr, 1000), target));//then  if !target.moneyMax exec grow -t 12
-    pids.push(ns.exec("bin.wk.js", attacker, Math.max(threadsWk, 1000), target));//then exec weaken 1 unitl target.moneyMax
+    pids.push(ns.exec("bin.gw.js", attacker, Math.max(threadsGr, 1), target));//then  if !target.moneyMax exec grow -t 12
+    pids.push(ns.exec("bin.wk.js", attacker, Math.max(threadsWk, 1), target));//then exec weaken 1 unitl target.moneyMax
     await WaitPids(ns, pids);
   }
 }
